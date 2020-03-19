3d6b92f694d656d0bc92705ed8cb7018fb7fce21
AbubakarManzoor <39098674+AbubakarManzoor@users.noreply.github.com>
2020-03-19 16:49:40 +0500

    Add project files.

---
+++ LoginRegister/LoginRegister/Views/LoginPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LoginRegister.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    [assembly: Dependency(typeof(MyDemo.Droid.CustomRenderers.Statusbar))]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            NavigationPage.SetHasNavigationBar(this, false);
+            InitializeComponent();
+            recoverhere.GestureRecognizers.Add(new TapGestureRecognizer()
+            {
+                Command = new Command(() =>
+                {
+                    DisplayAlert("Task", "youclicked on me", "ok");
+                })
+            });
+
+            signupLable.GestureRecognizers.Add(new TapGestureRecognizer()
+            {
+                Command = new Command(() =>
+                {
+                    gettingToRegisterScreen();
+                })
+            });
+
+
+        }
+
+
+        private void LoginButtonClicked(object sender, EventArgs e)
+        {
+            Device.BeginInvokeOnMainThread(async () =>
+            {
+                await this.DisplayAlert("Error", "Enter the UserName", "Cancle");
+            });
+
+        }
+
+
+        private async void Button_ClickedAsync(object sender, EventArgs e)
+        {
+            await this.Navigation.PushAsync(new Register());
+        }
+
+        private async void gettingToRegisterScreen()
+        {
+            await this.Navigation.PushAsync(new Register());
+        }
+    }
+}
---
+++ LoginRegister/LoginRegister/Views/Register.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LoginRegister.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Register : ContentPage
+    {
+        public Register()
+        {
+            NavigationPage.SetHasNavigationBar(this, false);
+            InitializeComponent();
+        }
+
+        public void SignupButtonClicked(object sender, EventArgs e)
+        {
+            goingToNextPage();
+            /*var username = SignupEntryUsername.Text;
+            var password = SignupEntryPasword.Text;
+            var email = SignupEntryPasword.Text;
+            var phoneNumber = SignupEntryPhoneNumber.Text;
+
+            if (username.Equals(""))
+            {
+                Device.BeginInvokeOnMainThread(async () =>
+                {
+                    await this.DisplayAlert("Error", "Enter the UserName", "Cancle");
+                });
+            }
+            else if (password.Equals(""))
+            {
+                Device.BeginInvokeOnMainThread(async () =>
+                {
+                    await this.DisplayAlert("Error", "Enter the password", "Cancle");
+                });
+            }
+            else if (email.Equals(""))
+            {
+                Device.BeginInvokeOnMainThread(async () =>
+                {
+                    await this.DisplayAlert("Error", "Enter the Email", "Cancle");
+                });
+            }
+            else if (phoneNumber.Equals(""))
+            {
+                Device.BeginInvokeOnMainThread(async () =>
+                {
+                    await this.DisplayAlert("Error", "Enter the Phone number", "Cancle");
+                });
+            }
+            else
+            {
+                goingToNextPage();
+            }*/
+
+        }
+
+        private async void goingToNextPage()
+        {
+            await this.Navigation.PushAsync(new PhoneNumberVarification());
+        }
+    }
+}
